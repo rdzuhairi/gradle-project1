e11903abafc6e5e8d7ab336955c250a455f7bf89
Zuhairi Rahmad <zuhairi.bit@gmail.com>
2018-10-15 23:36:37 +0900

    First commit

---
+++ src/main/java/entity/DBConnection.java
@@ -0,0 +1,48 @@
+package entity;
+public class DBConnection {
+    private String host;
+    private String port;
+    private String dbName;
+    private String dbUser;
+    private String dbPwd;
+
+    public String getHost() {
+        return host;
+    }
+
+    public void setHost(String host) {
+        this.host = host;
+    }
+
+    public String getPort() {
+        return port;
+    }
+
+    public void setPort(String port) {
+        this.port = port;
+    }
+
+    public String getDbName() {
+        return dbName;
+    }
+
+    public void setDbName(String dbName) {
+        this.dbName = dbName;
+    }
+
+    public String getDbUser() {
+        return dbUser;
+    }
+
+    public void setDbUser(String dbUser) {
+        this.dbUser = dbUser;
+    }
+
+    public String getDbPwd() {
+        return dbPwd;
+    }
+
+    public void setDbPwd(String dbPwd) {
+        this.dbPwd = dbPwd;
+    }
+}
---
+++ src/main/java/entity/Page.java
@@ -0,0 +1,12 @@
+package entity;
+
+import java.util.List;
+
+public class Page {
+    public int id;
+    public String home_page;
+    public List<String> list_page;
+    public String database_table;
+    public String file_folder;
+    public String selector;
+}
